a7c8fb8269777e5cf4dcfcbe1d7068f3d1f0cf28
shaikhshehzad <shehzadali3113@gmail.com>
2022-02-02 16:31:26 +0530

    mounting

--- src/components/practice_components/Parent.js
+++ src/components/practice_components/Parent.js
@@ -2,7 +2,85 @@ import React from "react";
 import Child01 from './Child01';
 
 class Parent extends React.Component{
+
+
+// Users = [
+//     { name: "shehzad" , Company : "Sears"},
+//     { name: "shehzad" , Company : "Sears"},
+//     { name: "shehzad" , Company : "Sears"},
+//     { name: "shehzad" , Company : "Sears"},
+//     { name: "shehzad" , Company : "Sears"}
+
+// ]
+
+constructor(){
+    super();
+    this.state ={
+        listofusers : [
+            { name: "shehzad" , Company : "Sears"},
+            { name: "shehzad" , Company : "Sears"},
+            { name: "shehzad" , Company : "Sears"},
+            { name: "shehzad" , Company : "Sears"},
+            { name: "shehzad" , Company : "Sears"}
+        
+        ],
+
+        countofusers :  5 ,
+        school : "Edureka",
+        stack : "Full Stack "
+
+    };
+    console.log("Constructor");
+}
+
+
+
+static getDerivedStateFromProps(props , state){
+    debugger
+    console.log("getDerivedStateFromProps")
+    state ={
+        ...state,
+        ...props
+    }
+    return state;
+    
+}
+
+componentDidMount(){
+    console.log("componentDidMount")
+}
+
+
+increaseCount = () =>{
+    //  this.state.countofusers ++ ;
+    // this.setState.countofusers ++;
+
+    this.setState({
+        countofusers : ++this.state.countofusers 
+    })
+
+    console.log(this.state.countofusers)
+
+}
+    
+decreaseCount = () =>{
+
+    console.log(this.state.countofusers)
+
+    this.setState({
+        countofusers : --this.state.countofusers 
+    })
+
+
+    // this.state.countofusers  -- ;
+
+     
+}
+
+ 
+
     render(){
+        console.log("render method")
         return(
             <>
             <h1> </h1>
@@ -10,11 +88,18 @@ class Parent extends React.Component{
                 <Child01/>
                 
 
-                {/* <Child02/> */}
 
                 <h1>{this.props.children}-{this.props.AN} </h1>
+                <h6>{this.state.countofusers}</h6>
+
+
+                <button onClick={this.increaseCount} >Increase</button>
+                <button onClick={this.decreaseCount}>Decrease</button>
+
+              
             </>
         )
     }
 }
 export default Parent;
+                // {/* <Child02/> */}
